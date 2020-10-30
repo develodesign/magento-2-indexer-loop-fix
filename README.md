db80283ea4cebc0aa480c3e4f470337924eb7ee6
Luke <luke@develodesign.co.uk>
2020-10-30 23:54:21 +0000

    Module implements the Indexer invalid loop bug fix

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # magento-2-indexer-invalid-loop-fix
-Magento Indexer Loop Fix
+Fixes the Magento Indexer Loop bug
+https://github.com/magento/magento2/pull/29196
+PR isnt' being merged until 2.4.2, this is tested on 2.3.5-p1
---
+++ app/code/Develo/IndexerLoopFix/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Develo All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Develo_IndexerLoopFix', __DIR__);
+
